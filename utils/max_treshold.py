c7df592deca1238ee252e083491a3fc1f9c7c821
Guillermo Tafoya <A01633790@tec.mx>
2023-12-13 21:37:07 -0500

    modified max threshold

--- utils/max_treshold.py
+++ utils/max_treshold.py
@@ -2,23 +2,21 @@ import torch
 
 # Inspired by https://arxiv.org/pdf/2012.04828.pdf
 
-def maximum_probability_thresholding_torch(predictions, lambda_values, p_value):
-    """
-    Apply maximum probability thresholding using PyTorch to assign pseudo labels.
+"""
+def maximum_probability_thresholding(logits, threshold=0.9):
     
-    :param predictions: PyTorch tensor of shape (n_samples, n_classes) containing predicted probabilities.
-    :param lambda_values: PyTorch tensor of shape (n_classes,) containing thresholds for each class.
-    :param p_value: float, portion of the most confident predictions to consider for setting thresholds.
-    
-    :return: PyTorch tensor of pseudo labels.
-    """
-    # Adjusting lambda_values based on p_value if needed
-    # This step is context-specific and depends on how you calculate or update lambda_values
+    probabilities = F.softmax(logits, dim=1)
+    max_probs, pseudo_labels = torch.max(probabilities, 1)
+    mask = max_probs > threshold
+
+    return pseudo_labels, mask
+"""
+
+
+def maximum_probability_thresholding_torch(predictions, lambda_values):
 
     max_probs, max_classes = torch.max(predictions, dim=1)
-
-    # Comparing max probabilities with lambda threshold for each class
     pseudo_labels = torch.where(max_probs > lambda_values[max_classes], max_classes, torch.tensor(-1).to(predictions.device))
-
+    
     return pseudo_labels
 
