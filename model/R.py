2d6664d487ca6d431f67477c77ca1c8066a9608a
Rudolph Pienaar <tafoyamilo@gmail.com>
2023-12-13 19:04:33 -0500

    Further implementation on E module and R module

--- model/R.py
+++ model/R.py
@@ -1,22 +1,23 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
-torch.nn.PixelShuffle
+import numpy as np
 
 # Given 
 class r_thingy(nn.Module):
     def __init__(self):
+        super(r_thingy, self).__init__()  
         self.pixelShuffle1 = nn.PixelShuffle(2)
         self.pixelShuffle2 = nn.PixelShuffle(2)
-        self.bilinear_interpolation_layer = nn.UpsamplingBilinear2d(size=(320,320))
+        #self.bilinear_interpolation_layer = F.interpolate(size=(320,320))
 
-    def forward(self, dict_feats, x):
+    def forward(self, x):
+        dict_feats = dict()
         x = self.pixelShuffle1(x)
         dict_feats['step1'] = x
         x = self.pixelShuffle2(x)
         dict_feats['step2'] = x
-        x = self.bilinear_interpolation_layer(x)
+        #x = F.interpolate(x,size=(320,320),mode='bilinear')
         return (x,dict_feats)
     
 
@@ -29,7 +30,12 @@ def normal_init(m, mean, std):
 class pdd(nn.Module):
     # initializers
     def __init__(self, d=64):
+        """
+        five convolutional layers with 4 x 4 convolutional kernel size, 
+        where the channel number is {64, 128, 256, 512, 1}, respectively
+        """
         super(pdd, self).__init__()
+        """
         self.conv1 = nn.Conv2d(6, d, 4, 2, 1)
         self.conv2 = nn.Conv2d(d, d * 2, 4, 2, 1)
         self.conv2_bn = nn.BatchNorm2d(d * 2)
@@ -38,12 +44,31 @@ class pdd(nn.Module):
         self.conv4 = nn.Conv2d(d * 4, d * 8, 4, 1, 1)
         self.conv4_bn = nn.BatchNorm2d(d * 8)
         self.conv5 = nn.Conv2d(d * 8, 1, 4, 1, 1)
+        """
+        self.step1 = nn.Sequential(
+            nn.ConvTranspose2d(1, 64, kernel_size=4),
+            nn.LeakyReLU(.02)
+        )
+        self.step2 = nn.Sequential(
+            nn.ConvTranspose2d(64, 128, kernel_size=4),
+            nn.LeakyReLU(.02)
+        )
+        self.step3 = nn.Sequential(
+            nn.ConvTranspose2d(128, 256, kernel_size=4),
+            nn.LeakyReLU(.02)
+        )
+        self.step4 = nn.Sequential(
+            nn.ConvTranspose2d(256, 512, kernel_size=4),
+            nn.LeakyReLU(.02)
+        )
+        self.step5 = nn.ConvTranspose2d(512, 1, kernel_size=4)
 
     # weight_init
     def weight_init(self, mean, std):
         for m in self._modules:
             normal_init(self._modules[m], mean, std)
 
+    """
     # forward method
     def forward(self, input, label):
         x = torch.cat([input, label], 1)
@@ -54,10 +79,20 @@ class pdd(nn.Module):
         x = F.sigmoid(self.conv5(x))
 
         return x
+    """
+    def forward(self, x):
+        x = self.step1(x)
+        x = self.step2(x)
+        x = self.step3(x)
+        x = self.step4(x)
+        x = self.step5(x)
+        return x
     
 if __name__ == '__main__':
-    from torchsummary import summary
+    #from torchsummary import summary
+    from torchinfo import summary
+    #model = Net().to(device)
     rmodel = r_thingy()
     pddmodel = pdd()
-    summary(rmodel, (1, 80, 80), device='cpu')
+    summary(rmodel, input_size=(16, 80, 80), device='cpu')
     summary(pddmodel, (1, 320, 320), device='cpu')
