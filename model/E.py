ab53477e51b37ccdef8d996918aea9f30376b21b
Rudolph Pienaar <tafoyamilo@gmail.com>
2024-01-17 11:21:14 -0500

    Further progress made, latent features extracted

--- model/E.py
+++ model/E.py
@@ -1,36 +1,36 @@
 """ Full assembly of the parts to form the complete network """
 
-import torch.nn.functional as F
 import torch.nn as nn
-import torch
 
 from config_parser import config
-from unet.unet_network import unet_network as Unet_network
+from unet.unet_network import Unet_network
 
 parser = config()
 args = parser.parse_args()
 
 class Module_e(nn.Module):
-    def __init__(self, n_channels, n_classes):
+    def __init__(self,
+                 axi,
+                 isize = [160, 160], 
+                 iaxis = [7,7,4], 
+                 loss = 'dice_loss', 
+                 coef = 'dice_coef', 
+                 style = 'basic', 
+                 ite = 3, 
+                 depth = 4, 
+                 dim = 32, 
+                 init = 'he_normal', 
+                 acti = 'elu', 
+                 lr = 1e-4
+                 ):
         super(Module_e, self).__init__()
-        self.n_channels = n_channels
-        self.n_classes = n_classes
-        model = Unet_network([*args.isize,1], args.iaxis[0], loss=args.loss, metrics=args.coef, style=args.style, 
-                                  ite=args.ite, depth=args.depth, dim=args.dim, init=args.init, acti=args.acti, lr=args.lr).build()
-        self.model = model.load_weights(args.axi)
-
-        self.conv = nn.Conv2d(n_channels, n_channels, kernel_size=3, stride=2)
+        model = Unet_network([*isize,1], iaxis[0], loss=loss, metrics=coef, style=style, 
+                                  ite=ite, depth=depth, dim=dim, init=init, acti=acti, lr=lr).build()
+        self.model = model.load_weights(axi)
         
+
     def forward(self, x):
-        # Perform segmentation
-        _ = self.model(x)
-
-        # Access the stored deepest feature map
-        deepest_features = self.model.deepest_feature_map
-        deepest_features_numpy = deepest_features.numpy()
-        deepest_features_torch = torch.from_numpy(deepest_features_numpy)
-
-        return deepest_features_torch
-
-
-
+        
+        return logits
+    
+test_dic, _ =make_dic(img_list, img_list, isize, 'axi',max_shape=max_shape)
