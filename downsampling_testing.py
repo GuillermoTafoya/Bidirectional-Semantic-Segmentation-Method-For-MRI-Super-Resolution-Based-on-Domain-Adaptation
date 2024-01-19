4daeb23a7875550ea417a6d87038f59a60b2dced
Rudolph Pienaar <tafoyamilo@gmail.com>
2024-01-19 11:13:26 -0500

    downsampling testing

--- downsampling_testing.py
+++ downsampling_testing.py
@@ -1,6 +1,6 @@
 import nibabel as nib
 import numpy as np
-from model.utils.upsample import upsample
+from model.utils.downsample import block_mean
 import matplotlib.pyplot as plt
 import torch 
 
@@ -13,25 +13,21 @@ def adjust_img(img):
         img = np.rot90(img)
         img = np.fliplr(img)
         return img
-"""
 
-for i, (dim, label) in enumerate([(0, 'Axial'), (1, 'Sagittal'), (2, 'Coronal')]):
-        input_slice = np.take(data, data.shape[i] // 2, axis=i)
-
-        input_slice = adjust_img(input_slice)
-
-        axarr[i, 0].imshow(input_slice, cmap='gray')
-
-
-        torch_slice
-
-plt.tight_layout()
-plt.savefig('test', bbox_inches='tight', pad_inches=0.1)
-plt.show()"""
+def pad_to_square(img):
+    h, w = img.shape
+    diff = abs(h-w)
+    padding = diff // 2
+    if h > w:
+        padded_img = np.pad(img, ((0, 0), (padding, padding)), mode='constant')
+    else:
+        padded_img = np.pad(img, ((padding, padding), (0, 0)), mode='constant')
+    return padded_img
 
 # Process and visualize one slice for demonstration
 input_slice = np.take(data, data.shape[0] // 2, axis=0)
 input_slice = adjust_img(input_slice)
+#input_slice = pad_to_square(input_slice)
 
 # Convert numpy array to torch tensor and add batch and channel dimensions
 torch_slice = torch.from_numpy(input_slice.copy()).unsqueeze(0).unsqueeze(0).float()
@@ -40,21 +36,21 @@ print(torch_slice.shape)
 print(torch_slice)
 
 # Upsample the slice
-upsampled_slice = upsample(torch_slice)
+downsampled_slice = block_mean(torch_slice)
 
 
 
 # Convert the upsampled tensor back to numpy for visualization
-upsampled_slice_np = upsampled_slice.squeeze().cpu().numpy()
+downsampled_slice_np = downsampled_slice.squeeze().cpu().numpy()
 
 # Plotting
 fig, axarr = plt.subplots(1, 2, figsize=(10, 5))
 axarr[0].imshow(input_slice, cmap='gray')
 axarr[0].set_title('Original Slice')
 
-axarr[1].imshow(upsampled_slice_np, cmap='gray')
-axarr[1].set_title('Upsampled Slice')
+axarr[1].imshow(downsampled_slice_np, cmap='gray')
+axarr[1].set_title('Downsampled Slice')
 
 plt.tight_layout()
-plt.savefig('Upsampling_test', bbox_inches='tight', pad_inches=0.1)
+plt.savefig('Downsampling_test', bbox_inches='tight', pad_inches=0.1)
 plt.show()
